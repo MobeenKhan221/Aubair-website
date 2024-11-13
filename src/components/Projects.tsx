75c35fd5150a2d361f72e416f361f6c6c8b39cfa
Mobeen Ahmad <Mobeen0096@gmail.com>
2024-11-13 12:09:42 +0500

    Projects Section Updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,25 +1,11 @@
 import "@/style/projets.css";
-import Image1 from "@/assets/Mask group.png";
-import Image2 from "@/assets/Mask group (1).png";
+// import Image1 from "@/assets/Mask group.png";
+// import Image2 from "@/assets/Mask group (1).png";
+import Projects1Line from "@/assets/projects-1-line.svg";
+import Projects2Line from "@/assets/projects-2-line.svg";
+// import Projects3Line from "@/assets/prjects-3-line.svg";
+// import Video1 from "@/assets/video.mp4";
 import Image from "next/image";
-
-const projects = [
-  {
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    imageSrc: Image1,
-  },
-  {
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    imageSrc: Image2,
-  },
-  {
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    imageSrc: Image1,
-  },
-];
 
 export default function Projects() {
   return (
@@ -27,21 +13,96 @@ export default function Projects() {
       <div className="projects-inner">
         <h1>Projects</h1>
         <div className="projects-all">
-          {projects.map((project, index) => (
-            <div
-              key={index}
-              className={`project-image-section ${
-                index === 1 ? "reverse" : ""
-              }`}
-            >
+          {/* Project 1 */}
+          <div className="project-image-section-main">
+            <div className="image-text-inner">
               <div className="projects-text">
-                <p>{project.description}</p>
+                <h2>Projects Name</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry&apos;s
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
               </div>
+
               <div className="image-div">
-                <Image src={project.imageSrc} alt="project image" />
+                {/* <Image src={Image1} alt="Project 1 Image" /> */}
               </div>
             </div>
-          ))}
+            <div className="project-video-line-main">
+              <div className="project-line">
+                <Image src={Projects1Line} alt="Project 1 Line" />
+              </div>
+              <div className="video-div">
+                <video controls>
+                  {/* <source src={Video1} type="video/mp4" /> */}
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </div>
+          </div>
+
+          <div className="project-image-section-main-2">
+            <div className="image-text-inner-2">
+              <div className="projects-text">
+                <h2>Projects Name</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry&apos;s
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+
+              <div className="image-div">
+                {/* <Image src={Image1} alt="Project 1 Image" /> */}
+              </div>
+            </div>
+            <div className="project-video-line-main-2">
+              <div className="project-line-2">
+                <Image src={Projects2Line} alt="Project 1 Line" />
+              </div>
+              <div className="video-div">
+                <video controls>
+                  {/* <source src={Video1} type="video/mp4" /> */}
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </div>
+          </div>
+
+          <div className="project-image-section-main">
+            <div className="image-text-inner">
+              <div className="projects-text">
+                <h2>Projects Name</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry&apos;s
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+
+              <div className="image-div">
+                {/* <Image src={Image1} alt="Project 1 Image" /> */}
+              </div>
+            </div>
+            <div className="project-video-line-main">
+              <div className="project-line">
+                <Image src={Projects1Line} alt="Project 1 Line" />
+              </div>
+              <div className="video-div">
+                <video controls>
+                  {/* <source src={Video1} type="video/mp4" /> */}
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

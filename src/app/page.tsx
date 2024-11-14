92507e7c7b171eafee4106cffc03e6346a786176
Mobeen Ahmad <Mobeen0096@gmail.com>
2024-11-14 19:07:35 +0500

    Changess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import Header from "@/components/header";
 import HomeSection from "@/components/Home-section";
-// import OurServices from "@/components/OurServices";
+import OurServices from "@/components/OurServices";
 import SkillsTechnology from "@/components/SkillsTechnology";
-// import Projects from "@/components/Projects";
+import Projects from "@/components/Projects";
 import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div>
       <Header />
       <HomeSection />
-      {/* <OurServices /> */}
+      <OurServices />
       <SkillsTechnology />
-      {/* <Projects /> */}
+      <Projects />
       <Footer />
     </div>
   );

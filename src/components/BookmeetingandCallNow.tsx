40a0f4b9e1b11e3d422a570b419cdf8e8a25e7ef
Mobeen Ahmad <Mobeen0096@gmail.com>
2024-11-19 18:55:30 +0500

    Design Complete and Functionality

--- src/components/BookmeetingandCallNow.tsx
+++ src/components/BookmeetingandCallNow.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { useState } from "react";
 import Image from "next/image";
 import BookmeetingIcon from "@/assets/Book-meetiing.svg";
@@ -7,77 +6,171 @@ import CallIcon from "@/assets/Call-icon.svg";
 import CallIconWhite from "@/assets/Call-icon-white.svg";
 import UploadIcon from "@/assets/Upload Icon.svg";
 import UploadIconWhite from "@/assets/Upload Icon-white.svg";
+import MainImaeg from "@/assets/meeting-and-call-main-image.svg";
+import ClosedIcon from "@/assets/Close-svg.svg";
 import "@/style/BookMeetingCallNow.css";
 
 export default function BookmeetingandCallNow() {
   const [isMeetingHovered, setIsMeetingHovered] = useState(false);
   const [isCallHovered, setIsCallHovered] = useState(false);
   const [isUploadHovered, setIsUploadHovered] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [link, setLink] = useState("");
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setUploadedFile(file); // Store the file
+    }
+  };
+
+  const handleLinkSubmit = () => {
+    if (link.trim() !== "") {
+      alert(`Link "${link}" submitted successfully!`);
+      setLink(""); // Clear input field
+    } else {
+      alert("Please enter a valid link.");
+    }
+  };
 
   return (
     <div className="bookmeeting-main">
       <div className="bookmeeting-inner">
-        <h2 className="heading">
-          Get Free Premium Audit/Feasibility study of your project ( Share your
-          Project detail Exclusively ){" "}
-        </h2>
+        <div className="meeting-inpus-main">
+          <h2 className="heading">
+            Get Free Premium Audit/Feasibility study of your project ( Share
+            your Project detail Exclusively ){" "}
+          </h2>
 
-        <div className="buttons-wrapper">
-          {/* Book A Meeting Button */}
-          <div className="button-container">
-            <a
-              href="https://calendly.com/sales-gun"
-              className="Bookmeeting-a-tag"
-              target="blank"
-            >
+          <div className="buttons-wrapper">
+            {/* Book A Meeting Button */}
+            <div className="button-container">
+              <a
+                href="https://calendly.com/sales-gun"
+                className="Bookmeeting-a-tag"
+                target="blank"
+              >
+                <button
+                  className="meeting-button"
+                  onMouseEnter={() => setIsMeetingHovered(true)}
+                  onMouseLeave={() => setIsMeetingHovered(false)}
+                >
+                  <span className="icon">
+                    <Image
+                      src={
+                        isMeetingHovered
+                          ? BookmeetingIconWhite
+                          : BookmeetingIcon
+                      }
+                      alt="Book Meeting Icon"
+                    />
+                  </span>
+                  Book A Meeting
+                </button>
+              </a>
+            </div>
+
+            {/* Call Now Button */}
+            <div className="button-container">
+              <a href="tel:+923488406670" className="Bookmeeting-a-tag">
+                <button
+                  className="call-button"
+                  onMouseEnter={() => setIsCallHovered(true)}
+                  onMouseLeave={() => setIsCallHovered(false)}
+                >
+                  <span className="icon">
+                    <Image
+                      src={isCallHovered ? CallIconWhite : CallIcon}
+                      alt="Call Icon"
+                    />
+                  </span>
+                  Call Now
+                </button>
+              </a>
+            </div>
+
+            {/* Drop the Docs Button */}
+            <div className="button-container">
               <button
-                className="meeting-button"
-                onMouseEnter={() => setIsMeetingHovered(true)}
-                onMouseLeave={() => setIsMeetingHovered(false)}
+                className="call-button"
+                onMouseEnter={() => setIsUploadHovered(true)}
+                onMouseLeave={() => setIsUploadHovered(false)}
+                onClick={() => setIsModalOpen(true)}
               >
                 <span className="icon">
                   <Image
-                    src={
-                      isMeetingHovered ? BookmeetingIconWhite : BookmeetingIcon
-                    }
-                    alt="Book Meeting Icon"
+                    src={isUploadHovered ? UploadIconWhite : UploadIcon}
+                    alt="Upload Icon"
                   />
                 </span>
-                Book A Meeting
+                Drop the Links / Docs
               </button>
-            </a>
+            </div>
           </div>
 
-          {/* Call Now Button */}
-          <div className="button-container">
-            <button
-              className="call-button"
-              onMouseEnter={() => setIsCallHovered(true)}
-              onMouseLeave={() => setIsCallHovered(false)}
-            >
-              <span className="icon">
+          {isModalOpen && (
+            <div className="modal">
+              <div className="modal-content">
                 <Image
-                  src={isCallHovered ? CallIconWhite : CallIcon}
-                  alt="Call Icon"
+                  className="close-button"
+                  onClick={() => setIsModalOpen(false)}
+                  style={{ color: "#000", width: "24px", height: "24px" }}
+                  src={ClosedIcon}
+                  alt="Close Icon"
                 />
-              </span>
-              Call Now
-            </button>
-          </div>
-          <div className="button-container">
-            <button
-              className="call-button"
-              onMouseEnter={() => setIsUploadHovered(true)}
-              onMouseLeave={() => setIsUploadHovered(false)}
-            >
-              <span className="icon">
-                <Image
-                  src={isUploadHovered ? UploadIconWhite : UploadIcon}
-                  alt="Call Icon"
-                />
-              </span>
-              (drop the link or File or doc or resume)
-            </button>
+                <h3>Upload Documents or Submit a Link</h3>
+
+                <div className="upload-section">
+                  <label htmlFor="file-upload" className="file-upload-label">
+                    <Image
+                      src={UploadIcon}
+                      alt="Upload Icon"
+                      width={50}
+                      height={50}
+                    />
+                    <input
+                      type="file"
+                      id="file-upload"
+                      accept=".pdf,.doc,.docx,.png,.jpg"
+                      onChange={handleFileUpload}
+                      className="file-input"
+                    />
+                    <span className="upload-text">Upload or Drop</span>
+                  </label>
+                  {uploadedFile && (
+                    <div className="preview-section">
+                      <p>Uploaded File: {uploadedFile.name}</p>
+                    </div>
+                  )}
+                </div>
+
+                <div className="link-section">
+                  <label htmlFor="link-upload">Submit a Link:</label>
+                  <input
+                    type="text"
+                    id="link-upload"
+                    placeholder="Enter your link here"
+                    value={link}
+                    onChange={(e) => setLink(e.target.value)}
+                  />
+                  <button onClick={handleLinkSubmit} className="modal-submit">
+                    Submit Link
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="Meeting-main-image">
+          <h1>High-impact services</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <div>
+            <Image src={MainImaeg} alt="Main Image" />
           </div>
         </div>
       </div>
